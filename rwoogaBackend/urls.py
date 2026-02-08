b20aeebb6b3f56ee2af39a3c5f0e11da7c08eaf9
Maccu <mariemaccu7@gmail.com>
2026-02-09 00:15:08 +0200

    resolving review comments

--- rwoogaBackend/urls.py
+++ rwoogaBackend/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-    path('api/products/', include('products.urls')),
+    path('api/v1/products/', include('products.urls')),
 
 ]
