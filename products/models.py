a0d9d5fde9ef3b65651ab45f0c5c49e945d8593e
mambatheo <muthegene61@gmail.com>
2026-02-24 10:53:35 +0200

    Custom R Updated

--- products/models.py
+++ products/models.py
@@ -252,6 +252,13 @@ class CustomRequest(models.Model):
     
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, 
+        on_delete=models.CASCADE, 
+        related_name="custom_requests", 
+        null=True, 
+        blank=True
+    )
 
     client_name = models.CharField(max_length=200)
     client_email = models.EmailField()
